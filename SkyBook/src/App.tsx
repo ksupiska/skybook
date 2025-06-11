a72bc0d7c7bd5a02a457311960bc97079658c377
Ksusha <https://github.com/ksupiska>
2025-06-11 23:50:34 +0300

    Added basic styles and button

--- SkyBook/src/App.tsx
+++ SkyBook/src/App.tsx
@@ -1,10 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { Container, Button } from 'react-bootstrap'
+import { BiSolidPencil } from "react-icons/bi";
+
+import './App.css'
 
 function App() {
 
   return (
     <>
-      <div>
-        
+      <div className='gradient'>
+        <Container>
+          <div className='container text-center'>
+            <h1 className='title'>SkyBook</h1>
+          </div>
+          <div className='container text-center'>
+            <h1 className="hello">Hello, what's on your mind today?</h1>
+            <Button className='entry-btn'>Add entry <BiSolidPencil/></Button>
+          </div>
+
+        </Container>
       </div>
     </>
   )
